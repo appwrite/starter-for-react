201d2667d4ad76ab26d8136820a3b62e91de74e8
ernstmul <ernst.mulders@gmail.com>
2025-04-28 16:05:18 +0200

    Fix: font poppins

--- src/App.jsx
+++ src/App.jsx
@@ -128,11 +128,11 @@ function App() {
             <span>Waiting for connection...</span>
           </div>
         ) : status === "success" ? (
-          <h1 className="text-2xl font-light text-[#2D2D31]">
+          <h1 className="font-[Poppins] text-2xl font-light text-[#2D2D31]">
             Congratulations!
           </h1>
         ) : (
-          <h1 className="font-[Poppins, arial, sans-serf] text-2xl font-light text-[#2D2D31]">
+          <h1 className="font-[Poppins] text-2xl font-light text-[#2D2D31]">
             Check connection
           </h1>
         )}
